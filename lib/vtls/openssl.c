23cf8f692c629ed15ce9d080ce8085458b147776
Aurimas Cernius <aurimasc@unity3d.com>
2016-12-23 11:55:48 +0200

    Fix winrt build

--- lib/vtls/openssl.c
+++ lib/vtls/openssl.c
@@ -103,7 +103,8 @@
 #endif
 
 #if (OPENSSL_VERSION_NUMBER >= 0x10100000L) && /* OpenSSL 1.1.0+ */ \
-  !defined(LIBRESSL_VERSION_NUMBER)
+  !defined(LIBRESSL_VERSION_NUMBER) \
+  && !defined(WINRT)
 #define SSLEAY_VERSION_NUMBER OPENSSL_VERSION_NUMBER
 #define HAVE_X509_GET0_EXTENSIONS 1 /* added in 1.1.0 -pre1 */
 #define HAVE_OPAQUE_EVP_PKEY 1 /* since 1.1.0 -pre3 */
