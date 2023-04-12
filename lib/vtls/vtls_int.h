f98866f4a848f31b2e00ecef6dce695da0a6b2d6
Aurimas Cernius <aurimasc@unity3d.com>
2023-04-12 14:46:13 +0300

    Readd lost unitytls.h include

--- lib/vtls/vtls_int.h
+++ lib/vtls/vtls_int.h
@@ -186,6 +186,7 @@ CURLcode Curl_ssl_addsessionid(struct Curl_cfilter *cf,
 #include "mbedtls.h"        /* mbedTLS versions */
 #include "bearssl.h"        /* BearSSL versions */
 #include "rustls.h"         /* rustls versions */
+#include "unitytls.h"
 
 #endif /* USE_SSL */
 
